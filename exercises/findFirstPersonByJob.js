7c8b6d57d0dc7c96d856469966e05466d70e843c
BigBrosh <maksuhin@gmail.com>
2020-05-08 06:54:32 +0300

    - added findFirstPersonByJob exercise.

---
+++ exercises/findFirstPersonByJob.js
@@ -0,0 +1,47 @@
+const usersGraph = {
+  Alice: {
+    job: 'CEO',
+    friends: ['Sam', 'Dean']
+  },
+  Bob: {
+    job: 'traveller',
+    friends: ['Dean']
+  },
+  Sam: {
+    job: 'hunter',
+    friends: ['Alice', 'Dean']
+  },
+  Dean: {
+    job: 'hunter',
+    friends: ['Alice', 'Sam', 'Bob']
+  },
+};
+
+const findFirstPersonByJob = (graph, user, job) => {
+  let queue = graph[user].friends;
+  const checked = new Set();
+
+  while(queue.length !== 0) {
+    const userName = queue[queue.length - 1];
+    const currentUser = graph[userName];
+
+    if (checked.has(userName)) {
+      queue.pop();
+      continue;
+    }
+
+    if (currentUser.job === job) {
+      return userName;
+    } else {
+      queue.pop();
+      checked.add(userName);
+      queue = queue.concat(currentUser.friends);
+    }
+  }
+
+  return null;
+};
+
+console.log(findFirstPersonByJob(usersGraph, 'Alice', 'traveller')); // -> Bob
+console.log(findFirstPersonByJob(usersGraph, 'Alice', 'hunter')); // -> Sam
+console.log(findFirstPersonByJob(usersGraph, 'Alice', 'developer')); // -> null
