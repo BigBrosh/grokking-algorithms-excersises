21cfa2f3b6c23c367bba1fa1261bccac996dabce
BigBrosh <maksuhin@gmail.com>
2020-05-10 11:07:42 +0300

    - added findShortestWeightedPath.

---
+++ exercises/findShortestWeightedPath.js
@@ -0,0 +1,56 @@
+// find shortest weighted path via Dijkstra's algorithm
+
+// Directed Acyclic Graph
+const weightedGraph = {
+  Begin: {
+    A: 6,
+    B: 2
+  },
+  A: {
+    End: 1
+  },
+  B: {
+    A: 3,
+    End: 5
+  }
+};
+
+const collectPathInfo = (graph, parent, to, pathInfo = {}) => {
+  const currentVertex = graph[parent];
+
+  Object.entries(currentVertex).forEach(([vertex, weight]) => {
+    const parentWeight = pathInfo[parent] ? pathInfo[parent].weight : 0;
+    const weightWithParent = weight + parentWeight;
+    const isVertexDefined = typeof pathInfo[vertex] !== 'undefined';
+    const isCurrentWeightLess = isVertexDefined && pathInfo[vertex].weight > weightWithParent;
+
+    if (!isVertexDefined || isCurrentWeightLess) {
+      pathInfo[vertex] = { parent, weight: weightWithParent };
+    }
+
+    if (vertex !== to) {
+      collectPathInfo(graph, vertex, to, pathInfo);
+    }
+  });
+
+  return pathInfo;
+};
+
+const findShortestWeightedPath = (graph, from, to) => {
+  const path = [to];
+  const pathInfo = collectPathInfo(graph, from, to);
+  const totalWeight = pathInfo[to].weight;
+  let current = pathInfo[to];
+
+  while (typeof current !== 'undefined') {
+    const { parent } = current;
+
+    path.unshift(parent);
+    current = pathInfo[parent];
+  }
+
+  return { totalWeight, path };
+};
+
+console.log(findShortestWeightedPath(weightedGraph, 'Begin', 'End'));
+// -> { totalWeight: 6, path: ['Begin', 'B', 'A', 'End'] }
